869eb022e99d63a7c6f4b1d9b6570161d4b95d10
Pham Tien Dung <dungkbkbhn@gmail.com>
2023-08-13 15:48:38 +0700

    Update

---
+++ src/EMSA.WebUI/Controllers/ApplicationBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMSA.WebUI.Controllers
+{
+    [ApiController]
+    [Route("emsa/[controller]")]
+    public class ApplicationBaseController : ControllerBase
+    {
+    }
+}
