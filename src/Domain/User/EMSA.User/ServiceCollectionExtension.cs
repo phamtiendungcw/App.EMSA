782adb7b07878b9985cbe589ddc03c643366a989
Pham Tien Dung <dungkbkbhn@gmail.com>
2023-08-14 22:18:19 +0700

    #8 - Service Token

---
+++ src/Domain/User/EMSA.User/ServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using EMSA.User.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EMSA.User
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddUserServices(this IServiceCollection services)
+        {
+            return services.AddScoped<IUserService, UserService>();
+        }
+    }
+}
