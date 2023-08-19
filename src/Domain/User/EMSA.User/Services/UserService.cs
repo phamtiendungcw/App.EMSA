5147ed7b8fde6754c5f07d1ba28fb2026a222258
Pham Tien Dung <dungkbkbhn@gmail.com>
2023-08-19 23:40:21 +0700

    Get user info for token generation

--- src/Domain/User/EMSA.User/Services/UserService.cs
+++ src/Domain/User/EMSA.User/Services/UserService.cs
@@ -10,6 +10,13 @@ namespace EMSA.User.Services
         /// <param name="user">Username and Password</param>
         /// <returns>true/false</returns>
         Task<bool> IsValidUserAccountAsync(UserLogin user);
+
+        /// <summary>
+        /// Lấy thông tin token của người dùng
+        /// </summary>
+        /// <param name="username">Tên người dùng</param>
+        /// <returns>token</returns>
+        Task<UserToken> GetUserTokenInfoAsync(string username);
     }
 
     public class UserService : IUserService
@@ -19,6 +26,11 @@ namespace EMSA.User.Services
         }
 
         public Task<bool> IsValidUserAccountAsync(UserLogin user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserToken> GetUserTokenInfoAsync(string username)
         {
             throw new NotImplementedException();
         }
--- src/EMSA.WebUI/Controllers/ApplicationBaseController.cs
+++ src/EMSA.WebUI/Controllers/ApplicationBaseController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EMSA.WebUI.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("emsa/[controller]")]
     public class ApplicationBaseController : ControllerBase
