8df1d6a6e921de494fd6736ebd783d768255ba57
Pham Tien Dung <dungkbkbhn@gmail.com>
2023-08-25 21:37:44 +0700

    Add ping controller

---
+++ src/EMSA.WebUI/Controllers/PingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMSA.WebUI.Controllers
+{
+    public class PingController : ApplicationBaseController
+    {
+        [Route("ping")]
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("ping");
+        }
+
+        [Route("ping-anonymous")]
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult PingAnonymous()
+        {
+            return Ok("ping anonymous");
+        }
+    }
+}
